21e6e7d1e60ef0ec1ac0915b7c98be99a29b962f
CreativeDream <yuliangagarin@mail.ru>
2015-04-08 09:37:42 +0200

    fix ie11 icon

--- js/jquery.filer.js
+++ js/jquery.filer.js
@@ -360,7 +360,8 @@
                                         '-webkit-box-shadow': h,
                                         '-moz-box-shadow': h,
                                         'box-shadow': h
-                                    });
+                                    }).attr('style', '-webkit-box-shadow: ' +  h + '; -moz-box-shadow: ' +  h + '; box-shadow: ' +  h + ';');
+                                    
                                     el = j.prop('outerHTML');
                                     j.remove();
                                 }
